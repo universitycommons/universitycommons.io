fb9ddbdea4b8b3fff19468b2913be6b230159d84
Logan Franken <loganfranken@gmail.com>
2016-08-06 21:50:01 -0700

    Implemented searching with mock results

--- public/script/main.js
+++ public/script/main.js
@@ -1,1 +1,159 @@
-document.getElementById('search-input').focus();
+var searchInput = document.getElementById('search-input');
+var searchForm = searchInput.parentElement;
+var mainContentArea = document.getElementById('content');
+
+if(searchInput && searchForm && mainContentArea)
+{
+  // Automatically focus search input
+  searchInput.focus();
+
+  // Hide "Search" button
+  searchForm.classList += ' minimal';
+
+  // Display search results as user types
+  addEventListener('keyup', function() {
+    onExecuteSearch(searchInput, mainContentArea);
+  });
+}
+
+// Coordinates the search request and display of results
+function onExecuteSearch(searchInput, resultsContainer)
+{
+  var query = searchInput.value;
+
+  if(query)
+  {
+    getResults(query, function(results) {
+
+      var resultsList = getResultsList(results);
+
+      // Clear existing results container (if applicable)
+      var currResultsList = document.getElementById('results');
+      if(currResultsList)
+      {
+        resultsContainer.removeChild(currResultsList);
+      }
+
+      // Display current results
+      resultsContainer.appendChild(resultsList);
+
+    });
+  }
+}
+
+// Executes the search for results
+function getResults(query, callback)
+{
+  var xhr = new XMLHttpRequest();
+
+  xhr.onreadystatechange = function()
+  {
+    if(xhr.readyState === 4)
+    {
+      var results;
+
+      try
+      {
+        results = JSON.parse(this.responseText)
+        callback(results);
+      }
+      catch(e)
+      {
+        // If we encounter an error while parsing the JSON,
+        // silently handle the error
+        callback([]);
+      }
+    }
+  }
+
+  xhr.open("GET", "/search?q=" + encodeURIComponent(query));
+  xhr.send();
+}
+
+// Displays the search results
+function getResultsList(results)
+{
+  // Results Container
+  var resultsContainer = elem('div', { class: 'container', id: 'results' });
+
+  // Results Header
+  resultsContainer.appendChild(elem('h2', { class: 'hidden', text: 'Results' }));
+
+  if(results.length === 0)
+  {
+    // "No Results" Message
+    return resultsContainer.appendChild(elem('p', { class: 'no-results-message', text: 'No results found' }));
+  }
+
+  // Project List
+  var projectList = elem('ul', { class: 'project-list' });
+
+  results.forEach(function(project) {
+
+    var projectListItem = elem('li', { class: 'project' });
+
+    // Project Name
+    projectListItem.appendChild(elem('span', { class: 'project-name', text: project.name }));
+
+    // Project Info
+    var projectInfo = elem('ul', { class: 'project-info' });
+    projectInfo.appendChild(projectInfoElem('project-category', 'Category', project.category));
+    projectInfo.appendChild(projectInfoElem('project-platform', 'Platform', project.platform));
+    projectInfo.appendChild(projectInfoElem('project-license', 'License', project.license));
+    projectInfo.appendChild(projectInfoElem('project-source', 'Source', project.sourceHost, project.sourceUrl));
+    projectListItem.appendChild(projectInfo);
+
+    // Project Description
+    projectListItem.appendChild(elem('p', { class: 'project-description', text: project.description }));
+
+    projectList.appendChild(projectListItem);
+
+  });
+
+  resultsContainer.appendChild(projectList);
+
+  return resultsContainer;
+}
+
+// Creates a project info element
+function projectInfoElem(className, title, value, url)
+{
+  // List Item
+  var listItem = elem('li', { class: className });
+
+  // Title
+  var infoTitle = elem('span', { class: 'info-title', text: title });
+  listItem.appendChild(infoTitle);
+
+  // Value
+  var infoValue = url
+   ? elem('a', { class: 'info-value', text: value, href: url })
+   : elem('span', { class: 'info-value', text: value });
+
+  listItem.appendChild(infoValue);
+
+  return listItem;
+}
+
+// Creates a DOM element
+function elem(elementName, options)
+{
+  var element = document.createElement(elementName);
+
+  if(options.class)
+  {
+    element.classList = options.class;
+  }
+
+  if(options.id)
+  {
+    element.id = options.id;
+  }
+
+  if(options.text)
+  {
+    element.textContent = options.text;
+  }
+
+  return element;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET Home Page */
+var mockSearchResults = [
+
+  {
+    name: 'Project Name 1',
+    category: 'Category Name 1',
+    platform: 'Platform Name 1',
+    license: 'MIT',
+    sourceHost: 'GitHub',
+    sourceUrl: 'https://github.com/universitycommons/universitycommons.io',
+    description: 'This is a description of the first project'
+  },
+
+  {
+    name: 'Project Name 2',
+    category: 'Category Name 2',
+    platform: 'Platform Name 2',
+    license: 'ISC',
+    sourceHost: 'SourceForge',
+    sourceUrl: 'https://github.com/universitycommons/universitycommons.io',
+    description: 'This is a description of the second project'
+  },
+
+];
+
+// Home
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'University Commons' });
+
+  var searchResults = {};
+  var showResults = false;
+  var searchInput = req.query.q;
+
+  if(typeof searchInput != 'undefined')
+  {
+    showResults = true;
+    searchResults = mockSearchResults;
+  }
+
+  res.render('index', {
+    title: 'University Commons',
+    showResults: showResults,
+    searchResults: searchResults
+  });
+
 });
 
+// About
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About > University Commons' });
+});
+
+// Search
+router.get('/search', function(req, res, next) {
+  res.json(mockSearchResults);
 });
 
 module.exports = router;
