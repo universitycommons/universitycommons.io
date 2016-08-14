a8e93f94a533551d95a253cb3ed75dbafe8782c6
Logan Franken <loganfranken@gmail.com>
2016-08-13 23:16:35 -0700

    Swapped categories with tags

--- data/category-alias.js
+++
@@ -1,7 +0,0 @@
-var CATEGORY = require('./category.js');
-
-var CATEGORY_ALIAS = {};
-CATEGORY_ALIAS[CATEGORY.LMS] = ['lms'];
-CATEGORY_ALIAS[CATEGORY.SIS] = ['sis'];
-
-module.exports = CATEGORY_ALIAS;
